8020b934309722b9ffb07ef8a3c7c89ded0bbc26
Pete Richardson <pete@peterichardson.com>
2022-07-18 10:59:09 -0700

    Simplify loadReminders into a sync async function.
    See also https://www.hackingwithswift.com/quick-start/concurrency/how-to-use-continuations-to-convert-completion-handlers-into-async-functions

--- asyncgetreminders/asyncgetreminders.swift
+++ asyncgetreminders/asyncgetreminders.swift
@@ -2,36 +2,39 @@
 //  main.swift
 //  asyncgetreminders
 //
+//  Loading reminders asynchronously in a finite command line tool without having the main thread
+//  sleep for a bit to let the reminders get loaded.
+//
+//  Use a continuation as described in
+//      https://www.hackingwithswift.com/quick-start/concurrency/how-to-use-continuations-to-convert-completion-handlers-into-async-functions
+//
 //  Created by Peter Richardson on 7/18/22.
 //
 
 import Foundation
 import EventKit
 
-
 @main
 struct AsyncGetReminders {
     let eventStore : EKEventStore
-        
-    func loadReminders(completion: @escaping ([EKReminder]) -> Void) {
+    
+    /// new intervening function that passes a continuation into the
+    /// old-style swift function that takes a completion handler.
+    func loadReminders() async -> [EKReminder] {
         guard let cal = eventStore.defaultCalendarForNewReminders() else {
-            completion([])
-            return
+            print("# ERROR: Could not get default calendar for new reminders !")
+            return []
         }
         let predicate = eventStore.predicateForReminders(in: [cal])
-        
-        eventStore.fetchReminders(matching: predicate) { foundReminders in
-            if let foundReminders {
-                completion(foundReminders.filter { !$0.isCompleted })
-                return
-            }
-            completion([])
-        }
-     }
-    
-    func loadReminders() async -> [EKReminder] {
-        await withCheckedContinuation { continuation in
-            loadReminders { reminders in
+
+        // wrap old-style function that takes a completion handler
+        // with an async block that uses a continuation
+        return await withCheckedContinuation { continuation in
+            eventStore.fetchReminders(matching: predicate) { foundReminders in
+                var reminders = [EKReminder]()
+                if let foundReminders {
+                    reminders = foundReminders.filter { !$0.isCompleted }
+                }
                 continuation.resume(returning: reminders)
             }
         }
@@ -39,14 +42,12 @@ struct AsyncGetReminders {
     
     init() {
         eventStore = EKEventStore()
-        eventStore.requestAccess(to: EKEntityType.reminder, completion: {
-            (granted, error) in
+        eventStore.requestAccess(to: EKEntityType.reminder, completion: { (granted, error) in
             if (!granted) || (error != nil) {
-                print("Reminder access denied!")
+                print("# ERROR: Access to reminders denied!")
                 exit(EXIT_FAILURE)
             }
         })
-        
     }
         
     func main() async throws {
